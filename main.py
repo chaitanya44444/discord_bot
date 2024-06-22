8824d69b9bb3029863fb61b5ad802f55688747ee
Chaitanya Vaswani <93338022+chaitanya44444@users.noreply.github.com>
2024-06-22 23:33:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,26 @@
-import discord
-from discord.ext import commands
-import asyncio  
+import nextcord
+from nextcord import Interaction
+from nextcord.ext import commands
 
-intents = discord.Intents.default()
-intents.members = True 
+intents = nextcord.Intents.default()
+intents.members = True  
 
-bot = commands.Bot(command_prefix=".", intents=intents)
 
-afk_status = {}
+client = commands.Bot(command_prefix="!", intents=intents)
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("The bot is now ready for use!")
+    print("...")  
 
-@bot.command()
-async def afk(ctx, reason=""):
-    afk_status[ctx.author.id] = reason
-    await ctx.channel.send(f"{ctx.author.mention} is now afk. {(reason if reason else '')}")
+test_server_id = ###############
 
-@bot.command()
-async def backfromafk(ctx):
-    if ctx.author.id in afk_status:
-        del afk_status[ctx.author.id]
-        await ctx.channel.send(f"{ctx.author.mention} is back from afk.")
-    else:
-        await ctx.channel.send(f"{ctx.author.mention} is not currently afk.")
+@client.slash_command(
+    name="test",
+    description="Introduction to Slash Commands",
+    guild_ids=[test_server_id]  
+)
+async def test(interaction: Interaction):
+    await interaction.response.send_message("Hello, subscribe please :)")
 
-@bot.command()
-async def emoji(ctx, emoji_name):
-    try:
-        custom_emoji = discord.utils.get(ctx.guild.emojis, name=emoji_name)
-        if custom_emoji:
-            await ctx.send(custom_emoji.url)
-            return
-
-        unicode_emoji = discord.utils.get(discord.emojis, name=emoji_name)
-        if unicode_emoji:
-            await ctx.send(f"https://snyk.io/advisor/python/emoji/functions/emoji.UNICODE_EMOJI.items") 
-            return
-
-        await ctx.send(f"Emoji '{emoji_name}' not found.")
-    except discord.HTTPException:
-        await ctx.send("Failed to retrieve emoji. Please check the emoji name.")
-
-@bot.event
-async def on_member_join(member):
-    
-    if member.id in afk_status:
-        del afk_status[member.id]
-        await member.guild.default_channel.send(f"{member.mention} is back from afk and the void like tf.")
-
-@bot.event
-async def on_message(message):
-    
-    if message.author.id in afk_status and message.author != bot.user:
-        del afk_status[message.author.id]
-        await message.channel.send(f"{message.author.mention} is back from afk. {(afk_status.get(message.author.id) if afk_status.get(message.author.id) else '')}")
-    await bot.process_commands(message)  s
-
-token = os.environ.get('BOT_TOKEN')  
-
-bot.run(token)
+client.run(BOT_TOKEN)
